da603405b619307a1830ba4d3b462f0298e3cb3f
deltaTreize <ludo.leblond13@gmail.com>
2024-06-20 17:52:31 +0200

    fix: correction updateCategoryInArray

--- server/services/userService.js
+++ server/services/userService.js
@@ -284,7 +284,7 @@ module.exports.updateCategoryInArray = async (serviceData) => {
 			throw new Error("User not found");
 		}
 		const categoryIndex = user.category.findIndex(
-			(data) => data.name === name
+			(data) => data.name === oldName
 		);
 
 		if (categoryIndex === -1) {
